e4bd16d33511464734494284718f808ca32be0ed
Marco Milanesi <kpanic@gmail.com>
2019-07-27 23:24:29 +0200

    Expand the basic API

--- lib/themoviedb.ex
+++ lib/themoviedb.ex
@@ -1,5 +1,13 @@
-defmodule Themoviedb do
-  def search(title, :movie) do
-    TMDB.movie(title)
+defmodule TheMovieDB do
+  alias TheMovieDB.API
+
+  def search(title, type, opts \\ [])
+
+  def search(title, :movie, recommendations: true) do
+    API.recommendations(title)
+  end
+
+  def search(title, :movie, _opts) do
+    API.movie(title)
   end
 end
